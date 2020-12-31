96d44c9c6438001bdcb4a6bd842b1c725010c8c5
ly <liangyong@chinamobiad.com>
2020-12-31 10:58:22 +0800

    last commit

--- app/src/main/java/com/ly/weiji/ui/MainActivity.java
+++ app/src/main/java/com/ly/weiji/ui/MainActivity.java
@@ -1,17 +1,8 @@
 package com.ly.weiji.ui;
 
 import android.os.Bundle;
-import android.support.v4.app.DialogFragment;
-import android.view.View;
 
-import com.appeaser.sublimepickerlibrary.datepicker.SelectedDate;
-import com.appeaser.sublimepickerlibrary.helpers.SublimeOptions;
-import com.appeaser.sublimepickerlibrary.recurrencepicker.SublimeRecurrencePicker;
 import com.ly.weiji.R;
-import com.ly.weiji.view.SublimePickerFragment;
-import com.orhanobut.logger.Logger;
-
-import java.util.Calendar;
 
 public class MainActivity extends BaseTitleActivity {
 
@@ -24,47 +15,6 @@ public class MainActivity extends BaseTitleActivity {
             loadRootFragment(R.id.fl_container, HomeFragment.newInstance());
         }
 
-        findViewById(R.id.tv_test).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                SublimePickerFragment pickerFrag = SublimePickerFragment.getInstance();
-                pickerFrag.setCallback(new SublimePickerFragment.Callback() {
-                    @Override
-                    public void onCancelled() {
-
-                    }
-
-                    @Override
-                    public void onDateTimeRecurrenceSet(SelectedDate selectedDate, int hourOfDay, int minute, SublimeRecurrencePicker.RecurrenceOption recurrenceOption, String recurrenceRule) {
-                        int year = selectedDate.getStartDate().get(Calendar.YEAR);
-                        int month = selectedDate.getStartDate().get(Calendar.MONTH);
-                        int day = selectedDate.getStartDate().get(Calendar.DAY_OF_MONTH);
-
-                        Logger.i(">>>>>>>year:" + year + " month:" + month + " day:" + day + " recurrenceRule:" + recurrenceRule);
-                    }
-                });
-
-                SublimeOptions options = new SublimeOptions();
-
-                Calendar startCal = Calendar.getInstance();
-                startCal.set(startCal.get(Calendar.YEAR) - 10, startCal.get(Calendar.MONTH), startCal.get(Calendar.DAY_OF_MONTH));
-                Calendar endCal = Calendar.getInstance();
-                endCal.set(endCal.get(Calendar.YEAR), endCal.get(Calendar.MONTH), endCal.get(Calendar.DAY_OF_MONTH));
-                //range
-                options.setDateParams(startCal, endCal);
-                // Enable/disable the date range selection feature
-                options.setCanPickDateRange(false);
-                options.setPickerToShow(SublimeOptions.Picker.DATE_PICKER);
-                options.setDisplayOptions(SublimeOptions.ACTIVATE_DATE_PICKER);
-
-                Bundle bundle = new Bundle();
-                bundle.putParcelable("SUBLIME_OPTIONS", options);
-                pickerFrag.setArguments(bundle);
-
-                pickerFrag.setStyle(DialogFragment.STYLE_NO_TITLE, 0);
-                pickerFrag.show(getSupportFragmentManager(), "SUBLIME_PICKER");
-            }
-        });
     }
 
     @Override
